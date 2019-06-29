f5d2c245c28afb248f386f12d6054043830c16ca
Ankit Agarwal <ankitakkii24@gmail.com>
2019-06-29 18:57:34 +0530

    welcome

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.be4c08d655aedd3dcd457d72cfa0a70c.js"
 );
 
 self.addEventListener('message', (event) => {
